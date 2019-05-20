26bd02b31908ebb8b046c192555d81dea9fc6a0a
Jocelyn MEYRON <Jocelyn.Meyron@grenoble-inp.org>
2019-05-20 18:12:14 +0200

    Add some skeleton

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, session
 
 app = Flask(__name__)
+
+@app.route("/register/")
+def register():
+    return render_template("register.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout/")
+def logout():
+    abort(404)
+
+@app.route("/user/<int:user_id>/")
+def user(user_id):
+    abort(404)
 
 @app.route("/")
 def index():
