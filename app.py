debc885329fd7c97b0b5607802b5e4751fcb2f82
Jocelyn MEYRON <Jocelyn.Meyron@grenoble-inp.org>
2019-05-21 13:15:25 +0200

    Session based login and logout

--- app.py
+++ app.py
@@ -1,20 +1,55 @@
-from flask import Flask, render_template, abort, session
+from functools import wraps
+from flask import Flask, render_template, abort, session, redirect, url_for, request, flash
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+
+# TODO: database for users, hashing for passwords
+class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+users = [ User("nyorem", "lol") ]
+def user_exists(username, password):
+    for user in users:
+        if user.username == username and user.password == password:
+            return True
+    return False
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("username"):
+            redirect(url_for("login", next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/register/")
 def register():
     return render_template("register.html")
 
-@app.route("/login/")
+@app.route("/login/", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        form = request.form
+        if user_exists(form["username"], form["password"]):
+            session["username"] = form["username"]
+            return redirect(url_for("index"))
+        else:
+            flash("Wrong credentials")
     return render_template("login.html")
 
 @app.route("/logout/")
+@login_required
 def logout():
-    abort(404)
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
+# TODO
 @app.route("/user/<int:user_id>/")
+# @login_required
 def user(user_id):
     abort(404)
 
--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 source venv/bin/activate
 
 export FLASK_ENV="development"
+# Obtained with the following command: python -c 'import os; print(os.urandom(12).hex())'
+export FLASK_SECRET_KEY="48610aee4c5aad2f8faec3f5"
 flask run
