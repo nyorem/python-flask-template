01022b05a4c03b56cd0aa1795a539a775289f796
Jocelyn MEYRON <Jocelyn.Meyron@grenoble-inp.org>
2019-05-24 11:49:27 +0200

    Move app to its own folder

--- blog/__init__.py
+++ blog/__init__.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///table.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(app.instance_path, "table.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 source venv/bin/activate
 
+export FLASK_APP="blog"
 export FLASK_ENV="development"
 # Obtained with the following command: python -c 'import os; print(os.urandom(12).hex())'
 export FLASK_SECRET_KEY="48610aee4c5aad2f8faec3f5"
